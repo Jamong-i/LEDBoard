917206dba3230f81956348b60bc7545bcb299553
Jamong-i <smkim961211@gmail.com>
2022-12-13 16:55:47 +0900

    Feat(Ui): mainUi desine setting
    보드 메인화면 및 설정화면 디자인

---
+++ LEDBoard/SettingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingViewController.swift
+//  LEDBoard
+//
+//  Created by Jamong on 2022/12/13.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
